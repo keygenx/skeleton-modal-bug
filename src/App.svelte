f8bd46801c8d43a0f1ed1312aa5bbbb51aec8826
keygenx <ananth.jayapal@gmail.com>
2024-02-27 13:41:49 +0000

    first commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,43 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  import {Modal} from '@skeletonlabs/skeleton'
-  import { initializeStores } from '@skeletonlabs/skeleton';
+  import {Modal, type ModalSettings} from '@skeletonlabs/skeleton'
+  import { initializeStores, getModalStore } from '@skeletonlabs/skeleton';
 
   initializeStores();
 
   const modalStore = getModalStore();
 
+  function triggerFirstModal() {
+				modalStore.trigger({
+			type: 'confirm',
+			title: 'Confirmation',
+			body:'Confirm to show second modal',
+			response: async (r: string) => {
+        triggerSecondModal();
+			}
+		});
+	}
+
+	function triggerSecondModal() {
+    //
+		const waitOneMillisecondPromise = new Promise((resolve) => {
+			setTimeout(() => {
+				resolve('Done waiting for 1 millisecond');
+			}, 1000);
+		});
+
+		waitOneMillisecondPromise.then((result) => {
+			modalStore.trigger({
+				type: 'alert',
+				title: 'Triggered',
+				body: `${result}`,
+				buttonTextCancel: 'Close'
+			});
+		});
+	}
   
 </script>
 
-<Modal/>
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+  <Modal/>
+  <button class="btn-icon" on:click={triggerFirstModal}> Click</button>
+  
